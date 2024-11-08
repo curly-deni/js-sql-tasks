1e125008501fa017f7538d39b0ba3234260f028b
Danila Mikhalev <danila@dan-mi.ru>
2024-11-08 19:04:27 +0300

    solutions

--- 1/solution.js
+++ 1/solution.js
@@ -1,12 +1,27 @@
 import postgres from "postgres";
 
 const config = {
-  host: "127.0.0.1",
-  user: "postgres",
-  password: "",
-  port: 5432,
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "",
+    port: 5432,
 };
 
 // BEGIN (write your solution here)
+export default async function solution() {
+    const sql = postgres(config);
+
+    await sql`
+        CREATE TABLE IF NOT EXISTS articles (
+            title VARCHAR(255),
+            description VARCHAR(255)
+        );
+    `;
+
+    await sql`
+        INSERT INTO articles (title, description)
+        VALUES ('Тест', 'Тест');
+    `;
+}
 
 // END
--- 2/solution.js
+++ 2/solution.js
@@ -1,12 +1,33 @@
 import postgres from "postgres";
 
 const config = {
-  host: "127.0.0.1",
-  user: "postgres",
-  password: "",
-  port: 5432,
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "",
+    port: 5432,
 };
 
 // BEGIN (write your solution here)
+const solution = async (articles) => {
+    if (!Array.isArray(articles) || articles.length === 0) {
+        return [];
+    }
 
+    const sql = postgres(config);
+
+    const ids = [];
+
+    for (const article of articles) {
+        const result = await sql`
+            INSERT INTO articles (title, description)
+            VALUES (${article.title}, ${article.description}) RETURNING id;
+        `;
+
+        ids.push(result[0].id);
+    }
+
+    return ids;
+}
+
+export default solution;
 // END
